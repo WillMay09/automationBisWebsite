4a92c2fff5fb64a0d92dfcfac6cbe21922ac0446
William <wmayhood@outlook.com>
2025-10-02 18:27:56 -0500

    first commit, next.js setup +outline of landingPage

---
+++ homePage.tsx
@@ -0,0 +1,19 @@
+export default function HomePage() {
+  return (
+    <section className="flex items-center  justify-center min-h-screen bg-gray-400 ">
+      <div className="text-center max-w-3xl p-6 border-4 border-amber-500 rounded-sm">
+        <h1 className="text-6xl font-bold text-gray-900 mb-6">
+          Automation, AI Driven Solutions,Custom Software
+        </h1>
+        <p className="text-lg">
+          Building Robust and Scalable AI Integrated Solutions taloired to your
+          bussiness needs
+        </p>
+        {/* */}
+        <a className="inline-block px-6 py-3 m-6 rounded-2xl bg-orange-500 text-white font-medium">
+          View My Work
+        </a>
+      </div>
+    </section>
+  );
+}
