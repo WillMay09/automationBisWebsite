d6bb49a9620ec39a2585be546492735cb6c52d88
William <wmayhood@outlook.com>
2025-10-02 19:49:34 -0500

    hero page setup complete, coloring and style defined

--- homePage.tsx
+++ homePage.tsx
@@ -1,19 +1,43 @@
 export default function HomePage() {
   return (
-    <section className="flex items-center  justify-center min-h-screen bg-gray-400 ">
+    <section className="flex items-center text-white   justify-center min-h-screen bg-[#2a2a2a] ">
       <div className="text-center max-w-3xl p-6 border-4 border-amber-500 rounded-sm">
-        <h1 className="text-6xl font-bold text-gray-900 mb-6">
+        <h1 className="text-6xl font-bold mb-10">
           Automation, AI Driven Solutions,Custom Software
         </h1>
-        <p className="text-lg">
+        <p className="text-lg mb-10">
           Building Robust and Scalable AI Integrated Solutions taloired to your
           bussiness needs
         </p>
-        {/* */}
-        <a className="inline-block px-6 py-3 m-6 rounded-2xl bg-orange-500 text-white font-medium">
-          View My Work
-        </a>
+        {/* Schedule a meet*/}
+        <form className="flex items-center justify-center">
+          <div className="flex w-full shadow-md overflow-hidden">
+            <input
+              type="email"
+              placeholder="enter email"
+              className="flex-1 px-4 py-3 bg-[#4a4a4a] focus:outline-non"
+              required
+            ></input>
+            <button className="px-5 bg-[#fe6036] text-white font-medium shadow-md hover:bg-blue-700 transition-colors">
+              Schedule a Meeting
+            </button>
+          </div>
+        </form>
       </div>
     </section>
   );
+  {
+    /* <form>
+          <input
+            type="email"
+            placeholder="enter email"
+            className="w-full px-4 py-3 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          ></input>
+          <button className="inline-block px-6 py-3 m-6 rounded-2xl bg-orange-500 text-white font-medium shadow-md hover:bg-blue-700 transition-colors">
+            Schedule a Meeting
+          </button>
+        </form> 
+        */
+  }
 }
